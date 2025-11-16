d9f66ec06bdb6449343daf12c8ae049e088b0142
khanh <dkhanh0512@gmail.com>
2025-11-16 23:40:06 +0700

    OLH commit

---
+++ src/Mang2Chieu.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Mang2Chieu {
+  public static int[][] nhapMang(int dong, int cot) {
+    Scanner sc = new Scanner(System.in);
+    int[][] a = new int[dong][cot];
+    for (int i = 0; i < dong; i++) {
+      for (int j = 0; j < cot; j++) {
+        System.out.print("a[" + i + "][" + j + "] = ");
+        a[i][j] = sc.nextInt();
+      }
+    }
+    return a;
+  }
+  public static void xuatMang(int[][] a) {
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        System.out.print(a[i][j] + "\t");
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int dong, cot;
+    System.out.println("nhap hang ");
+    dong = sc.nextInt();
+    System.out.print("nhap cot ");
+    cot = sc.nextInt();
+    int[][] arr = nhapMang(dong, cot);
+    xuatMang(arr);
+  }
+}
