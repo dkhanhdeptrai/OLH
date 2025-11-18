cf59102230a989f7a83d24c66cf6989ccd564266
dkhanhdeptrai <111080381+dkhanhdeptrai@users.noreply.github.com>
2025-11-18 11:33:55 +0700

    fix: dùng method String
    Refactor to count digits and calculate their sum more efficiently.

--- src/DemSoVaChu.java
+++ src/DemSoVaChu.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
 public class DemSoVaChu {
-
   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     String xau = sc.nextLine();
-    int count = 0;
+    
+    String digits = xau.replaceAll("\\D", "");
+    int count = digits.length();
     int sum = 0;
-    for(int i = 0; i < xau.length(); i++) {
-      char c = xau.charAt(i);
-      if(Character.isDigit(c)) {
-        count++;
-        sum += c - '0';
-
-      }
+    for (char c : digits.toCharArray()) {
+      sum += c - '0';
     }
     System.out.println(count);
     System.out.println(sum);
